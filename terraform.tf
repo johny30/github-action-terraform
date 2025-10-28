5cc5a02fc6eb3a96094e6fe5a89eb41da4fe6a3a
Tejendra Sapkota <sapkota30@gmail.com>
2025-10-28 14:43:37 -0400

    Add Terraform config and github action deployment

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,15 @@
 terraform {
-required_version = ">= 1.3.0"
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 5.0"
-}
-backend "s3" {
-bucket = "aws-terraform.tfstate "
-key = "env/dev/terraform.tfstate"
-region = "ca-central-1"
-}
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "aws-terraform.tfstate" 
+    key    = "env/dev/terraform.tfstate"
+  } 
+} 
